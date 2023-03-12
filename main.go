bb7a2f2be057be860028eb0745c191e369fb9583
Julieta Bonazza <60350242+julieta-311@users.noreply.github.com>
2023-03-12 15:05:23 +0000

    Add insertion using squirrel (#1)
    * Update readme
    
    * Add insertion using squirrel
    
    * Run the thingie like a test

--- main.go
+++ main.go
@@ -2,55 +2,87 @@ package main
 
 import (
 	"context"
+	"database/sql"
+	"errors"
 	"fmt"
-	"log"
 	"os"
+	"time"
 
+	"github.com/Masterminds/squirrel"
 	"github.com/jackc/pgx/v5"
 )
 
 func main() {
+	if err := run(); err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Println("Success!")
+}
+
+func run() error {
 	connString := os.Getenv("POSTGRES_URL")
 	if connString == "" {
-		fmt.Println("POSTGRES_URL is required")
-		return
+		return errors.New("POSTGRES_URL is required")
 	}
 
 	ctx := context.Background()
 	conn, err := pgx.Connect(ctx, connString)
 	if err != nil {
-		fmt.Printf("failed to connect to db: %v.\n", err)
-		return
+		return fmt.Errorf("failed to connect to db: %w", err)
 	}
 	defer func() {
 		if err := conn.Close(ctx); err != nil {
-			log.Printf("failed to close db connection: %v.\n", err)
+			fmt.Printf("Failed to close db connection: %v.\n", err)
 		}
 	}()
 
-	q, err := os.ReadFile("./testdata/initial_schema.sql")
+	db := &db{conn: conn}
+
+	schemaMigrations, err := os.ReadFile("./testdata/initial_schema.sql")
 	if err != nil {
-		fmt.Printf("failed to read migrations file: %v.\n", err)
-		return
+		return fmt.Errorf("failed to read migrations file: %w", err)
 	}
 
-	tx, err := conn.Begin(ctx)
+	m := squirrel.Expr(string(schemaMigrations))
+	if _, err := squirrel.ExecContextWith(ctx, db, m); err != nil {
+		return fmt.Errorf("failed to run initial db migration: %w", err)
+	}
+
+	psql := squirrel.StatementBuilder.PlaceholderFormat(squirrel.Dollar)
+	insert := psql.Insert("thing").
+		Columns("name", "labels", "n", "x", "created_at", "stuff").
+		Values(
+			"Foo",
+			[]string{"cat", "dog"},
+			7,
+			1.283,
+			time.Now(),
+			`{"a": 7, "b": "yes"}`)
+
+	if _, err := squirrel.ExecContextWith(ctx, db, insert); err != nil {
+		return fmt.Errorf("failed to execute insertion: %w", err)
+	}
+
+	return nil
+}
+
+type db struct {
+	conn *pgx.Conn
+}
+
+func (d *db) ExecContext(ctx context.Context, query string, args ...any) (_ sql.Result, err error) {
+	tx, err := d.conn.Begin(ctx)
 	if err != nil {
-		fmt.Printf("failed to begin transaction: %v.\n", err)
-		return
+		return nil, fmt.Errorf("failed to begin transaction: %w", err)
 	}
 	defer tx.Rollback(ctx)
 
-	_, err = tx.Exec(ctx, string(q))
+	_, err = tx.Exec(ctx, query, args...)
 	if err != nil {
-		fmt.Printf("failed to execute initial db migration: %v.\n", err)
-		return
+		return nil, fmt.Errorf("failed to execute query %q with args %q: %w", query, args, err)
 	}
 
-	if err := tx.Commit(ctx); err != nil {
-		fmt.Printf("failed to commit initial db migration: %v.\n", err)
-		return
-	}
-
-	fmt.Println("Success")
+	return nil, tx.Commit(ctx)
 }
